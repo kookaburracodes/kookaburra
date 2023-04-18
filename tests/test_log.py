6baf3ea94bf3522960598a64af4b7f26f7f6da6b
Anthony Corletti <anthcor@gmail.com>
2023-04-18 00:26:19 -0400

    update test

--- tests/test_log.py
+++ tests/test_log.py
@@ -1,8 +1,9 @@
 from datetime import datetime
 from typing import Any
+from unittest.mock import Mock, patch
 from uuid import uuid4
 
-from kookaburra.log import log
+from kookaburra.log import log, setup_logging_queue
 
 
 def test_logger(capsys: Any) -> None:
@@ -27,3 +28,14 @@ def test_logger(capsys: Any) -> None:
     out, err = capsys.readouterr()
     assert out == ""
     assert "TypeError: Object of type MyUnsupportedType is not JSON serializable" in err
+
+
+@patch("logging.getLogger")
+def test_logger_not_root_handler(mock_logger: Mock) -> None:
+    mock_handlers = [Mock()]
+    mock_logger.return_value.handlers = mock_handlers
+
+    setup_logging_queue()
+
+    # Check if the original handlers have been removed from the logger
+    assert len(mock_logger.return_value.handlers) == 1
